e2b29bbd0d03078e179273da42cc1751b34f3d06
Dakota Nelson <kotalilyy@gmail.com>
2025-07-01 13:42:48 -0500

    specify width/height + create constructor

--- src/PacMan.java
+++ src/PacMan.java
@@ -5,6 +5,27 @@ import java.util.Random;
 import javax.swing.*;
 
 public class PacMan extends JPanel {
+    class Block {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image image;
+
+        int startX;
+        int startY;
+
+        Block(Image image, int x, int y, int width, int height) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width; 
+            this.height = height;
+            this.startX = x;
+            this.startY = y;
+        }
+    }
+
     private int rowCount = 21;
     private int columnCount = 19;
     private int tileSize = 32;
