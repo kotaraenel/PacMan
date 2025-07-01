a6426819b5e4e749046f2b9dbcd6cda6adb8ac07
Dakota Nelson <kotalilyy@gmail.com>
2025-07-01 10:12:18 -0500

    create window settings

--- src/App.java
+++ src/App.java
@@ -1,11 +1,18 @@
+import javax.swing.JFrame;
 
-import javax.swing.Jframe;
 public class App {
     public static void main(String[] args) throws Exception {
         int rowCount = 21;
         int columnCount = 19;
         int tileSize = 32;
-        int boardwidth = columnCount * tileSize;
+        int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
