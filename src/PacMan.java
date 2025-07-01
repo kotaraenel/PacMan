ac496bf03ceb76530e305ac031af7a321e4b0dc6
Dakota Nelson <kotalilyy@gmail.com>
2025-07-01 10:34:48 -0500

    inherit JPanel

--- src/PacMan.java
+++ src/PacMan.java
@@ -5,6 +5,14 @@ import java.util.Random;
 import javax.swing.*;
 
 public class PacMan extends JPanel {
-    
+    private int rowCount = 21;
+    private int columnCount = 19;
+    private int tileSize = 32;
+    private int boardWidth = columnCount * tileSize;
+    private int boardHeight = rowCount * tileSize;
 
+    PacMan() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+    }
 }
