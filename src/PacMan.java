25fd8ad71e4e61fc573f8f5532ffbdcdaeb9ed57
Dakota Nelson <kotalilyy@gmail.com>
2025-07-01 10:42:19 -0500

    create image variables

--- src/PacMan.java
+++ src/PacMan.java
@@ -11,6 +11,13 @@ public class PacMan extends JPanel {
     private int boardWidth = columnCount * tileSize;
     private int boardHeight = rowCount * tileSize;
 
+    private Image wallImage;
+    private Image blueGhostImage;
+    private Image orangeGhostImage;
+    private Image pinkGhostImage;
+    private Image redGhostImage;
+
+
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
