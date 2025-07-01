d7d7769c6c77850170bc0ac8b6648a1672b2ec12
Dakota Nelson <kotalilyy@gmail.com>
2025-07-01 10:23:32 -0500

    JPanel

--- src/PacMan.java
+++ src/PacMan.java
@@ -1,9 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Hashset;
+import java.util.HashSet;
 import java.util.Random;
 import javax.swing.*;
 
-public class PacMan {
+public class PacMan extends JPanel {
+    
 
 }
